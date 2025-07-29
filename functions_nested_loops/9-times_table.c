fa82f8545644ac7a600a855958b10ef5326a5cdc
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-07-29 15:57:10 +0400

    commit

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
         for (col = 0; col <= 9; col++)
         {
             product = row * col;
-            
+
             if (col == 0)
             {
                 _putchar('0');
