76c713c27f856bc9bb21f0b2377c881ffa528eb0
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-07-29 13:47:09 +0400

    commit

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-    char str[] = "_putchar\n";
-    int i = 0;
+	char str[] = "_putchar\n";
+	int i = 0;
 
-    while (str[i])
-    {
-        _putchar(str[i]);
-        i++;
-    }
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
-    return (0);
+	return (0);
 }
