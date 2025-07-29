9ce6430985fdbb1a087e320add45b328be543c14
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-07-29 15:56:33 +0400

    commit

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,15 +1,18 @@
 #include "main.h"
+
 /**
  * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
     int row, col, product;
+
     for (row = 0; row <= 9; row++)
     {
         for (col = 0; col <= 9; col++)
         {
             product = row * col;
+            
             if (col == 0)
             {
                 _putchar('0');
