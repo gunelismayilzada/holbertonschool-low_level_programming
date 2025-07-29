841bb807772c2ca30c943b3a848697a927b93a55
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-07-29 15:12:05 +0400

    commit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,20 +2,18 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: number to process
+ * @n: the number to process
  *
- * Return: last digit of n
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
     int last_digit;
 
-    if (n < 0)
-        n = -n;
+    last_digit = n % 10;      // get last digit (could be negative)
+    if (last_digit < 0)
+        last_digit = -last_digit;  // convert to positive if negative
 
-    last_digit = n % 10;
-
-    _putchar(last_digit + '0');  // convert digit to character
-
+    _putchar('0' + last_digit);  // print the digit character
     return (last_digit);
 }
