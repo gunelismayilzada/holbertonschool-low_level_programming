aa48b23d014455e0d74adca8b4040944ff1449f7
gunel <gunel@gmail.com>
2025-07-31 13:29:00 +0400

    test

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * You can only use _putchar twice in your code
+ * Prototype: void print_numbers(void);
+ */
+void print_numbers(void){
+    for (int i; i < 10; i++){
+        _putchar(i);
+        _putchar("\n");
+    }
+}
