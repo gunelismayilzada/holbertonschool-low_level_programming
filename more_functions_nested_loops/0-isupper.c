dfc5468e3fccb6d4eb6b4be08b04c67e1551e434
Your Name <you@example.com>
2025-07-31 12:24:02 +0400

    commit

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * Returns 1 if c is uppercase
+ * Returns 0 otherwise
+ */
+ int _isupper(int c)
+ {
+    if (c >= 'A' && c <= 'Z')
+        return(1);
+    return(0);
+ }
