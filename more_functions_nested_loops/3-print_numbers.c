989fa794fafc3a2616dc76e57acc323a3e0e89d9
gunel <gunel@gmail.com>
2025-07-31 13:48:45 +0400

    test

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * You can only use _putchar twice in your code
- * Prototype: void print_numbers(void);
+ * print_numbers - prints the numbers 0 to 9 followed by a new line
  */
 void print_numbers(void)
 {
     char c;
     for (c = 0; c <= 9; c++)
-        _putchar(c + '0');  // First use of _putchar (inside loop)
-    _putchar('\n'); 
-    
+        _putchar(c + '0'); /* convert number to ASCII digit */
+    _putchar('\n');
 }
