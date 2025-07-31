b289aac92b3199e00083caf957c29ac57c91623b
gunel <gunel@gmail.com>
2025-07-31 12:32:16 +0400

    test

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,9 +4,9 @@
  * Returns 1 if c is uppercase
  * Returns 0 otherwise
  */
- int _isupper(int c)
- {
+int _isupper(int c)
+{
     if (c >= 'A' && c <= 'Z')
-        return(1);
-    return(0);
- } 
+        return (1);
+    return (0);
+}
