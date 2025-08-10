e2c0ed6fb95d3cdfff0ecd5f840e237b850bb9fd
Gunel <11835@holbertonstudents.com>
2025-08-10 15:38:31 +0400

    commit

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: pointer to the string to print
+ *
+ * Description: This function takes a string 's' and prints its characters
+ * starting from the last character to the first, effectively printing the
+ * string in reverse order. After printing, it outputs a newline character.
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	/* Calculate the length of the string */
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	/* Print the string in reverse */
+	while (length > 0)
+	{
+		length--;
+		_putchar(s[length]);
+	}
+
+	/* Print a newline at the end */
+	_putchar('\n');
+}
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,6 +6,8 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char i);
+void print_rev(char *s);
+
 
 
  
