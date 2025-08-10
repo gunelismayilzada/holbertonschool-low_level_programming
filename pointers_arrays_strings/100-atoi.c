f2944f541e88ff4c9b7dba8080e0d9a58893dc7e
Gunel <11835@holbertonstudents.com>
2025-08-10 16:57:45 +0400

    commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <limits.h> /* for INT_MAX, INT_MIN */
 
 /**
- * _atoi - convert a string to an integer.
- * @s: pointer to the string to convert.
+ * _atoi - converts a string to an integer safely handling overflow.
+ * @s: input string.
  *
- * Return: converted integer, or 0 if none found.
+ * Return: converted integer or INT_MAX/INT_MIN on overflow.
  */
 int _atoi(char *s)
 {
@@ -13,22 +14,30 @@ int _atoi(char *s)
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-		{
 			sign = -sign;
-		}
 		else if (s[i] == '+')
-		{
-			/* sign unchanged */
-		}
+			;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			started = 1;
+
+			/* Check overflow for positive sign */
+			if (sign == 1)
+			{
+				if (num > (INT_MAX - (s[i] - '0')) / 10)
+					return (INT_MAX);
+			}
+			else
+			{
+				/* Check overflow for negative sign */
+				if (num > ((unsigned)INT_MAX + 1 - (s[i] - '0')) / 10)
+					return (INT_MIN);
+			}
+
 			num = num * 10 + (s[i] - '0');
 		}
 		else if (started)
-		{
 			break;
-		}
 		i++;
 	}
 
