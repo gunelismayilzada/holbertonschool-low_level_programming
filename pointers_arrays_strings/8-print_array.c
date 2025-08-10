33aefd2fc9c6655d13fff960b7f1f298be9c7d31
Gunel <11835@holbertonstudents.com>
2025-08-10 16:27:56 +0400

    commit

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -4,16 +4,18 @@
  * print_array - N element array çap edir
  * @a: array-in göstəricisi
  * @n: çap olunacaq element sayı
+ *
+ * Description: Array elementlərini vergül və boşluqla ayrılmış şəkildə çap edir
  */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", a[i]);
-        if (i != n - 1)
-            printf(", ");
-    }
-    printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	printf("\n");
 }
