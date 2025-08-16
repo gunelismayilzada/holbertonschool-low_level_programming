a958b7b54b95bf19914230b8ea78a10332433c4a
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-08-16 17:32:41 +0400

    commit

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string up to n bytes
+ * @dest: Destination buffer
+ * @src: Source string
+ * @n: Maximum number of bytes to copy
+ *
+ * Return: Pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,7 +23,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *leet(char *);
-
+char *_strncpy(char *dest, char *src, int n);
 
  
  #endif
