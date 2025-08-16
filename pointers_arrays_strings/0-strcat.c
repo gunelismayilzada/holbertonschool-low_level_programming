d95d0c10fd237ea896b9ba0a4ed47abda9fc484b
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-08-16 15:00:30 +0400

    commit

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,25 +9,25 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    /* Find the end of the dest string */
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
+	/* Find the end of the dest string */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-    /* Append src to dest */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	/* Append src to dest */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    /* Null-terminate the resulting string */
-    dest[i] = '\0';
+	/* Null-terminate the resulting string */
+	dest[i] = '\0';
 
-    return dest;
+	return (dest);
 }
