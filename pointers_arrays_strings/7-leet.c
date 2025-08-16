0c1462bcd9407103828570305329426117f2dfd1
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-08-16 17:28:46 +0400

    commit

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ * @s: The string to encode
+ *
+ * Return: A pointer to the resulting string
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+			}
+		}
+	}
+	return (s);
+}
