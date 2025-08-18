72cf8221ca87a1037b5292249fb3b242a2123705
Your Name <you@example.com>
2025-08-18 16:17:08 +0400

    test

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,7 +9,7 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	unsigned int sum1 = 0, sum2 = 0;
+	int sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
@@ -17,5 +17,5 @@ void print_diagsums(int *a, int size)
 		sum2 += *(a + i * size + (size - 1 - i)); /* Secondary diagonal */
 	}
 
-	printf("%u, %u\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
