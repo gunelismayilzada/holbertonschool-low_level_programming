4da55ae3013b5eace47a79da9ef5f459bf538c4c
Your Name <you@example.com>
2025-08-18 15:19:59 +0400

    test

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,5 +25,7 @@ char *leet(char *);
 char *leet(char *);
 char *_strncpy(char *dest, char *src, int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
  
  #endif
