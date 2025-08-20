71b2240368ac9b1391802afa53b2f1529949bcd7
Your Name <you@example.com>
2025-08-20 15:23:59 +0400

    commit

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
-*_print_rev_recursion - prints a string in reverse
-* @s: the string to print
-*/
-
+ * _print_rev_recursion - prints a string in reverse using recursion
+ * @s: the string to be printed
+ */
 void _print_rev_recursion(char *s)
 {
-  if (*s == '\0')  // Base case: end of string
-		return;
+    if (*s == '\0')  // Base case: end of string
+        return;
 
-	_print_rev_recursion(s + 1);  // Recursive call with next character
-	_putchar(*s);              
+    _print_rev_recursion(s + 1);  // Recursive call with next character
+    _putchar(*s);  // Print after recursive call (on the "way back")
 }
