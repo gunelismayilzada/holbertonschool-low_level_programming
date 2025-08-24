6029eb8a18b4e466080a2279424792814af5dd67
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-08-24 15:00:22 +0400

    commit

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,13 +2,14 @@
 
 /**
  * main - prints the program's name (argv[0])
- * @argc: argument count (not used)
+ * @argc: argument count (unused)
  * @argv: argument vector (array of strings)
+ *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-    (void)argc;          /* Silence unused parameter warning */
+    (void)argc; /* Silence unused parameter warning */
     printf("%s\n", argv[0]);
-    return 0;
+    return (0);
 }
