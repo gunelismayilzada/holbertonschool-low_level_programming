d898889bc4f30c8dc3844060dde51ebc65163c60
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-08-24 15:02:18 +0400

    commit

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-    (void)argc; /* Silence unused parameter warning */
-    printf("%s\n", argv[0]);
-    return (0);
+(void)argc; /* Silence unused parameter warning */
+printf("%s\n", argv[0]);
+return (0);
 }
