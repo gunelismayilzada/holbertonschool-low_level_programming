13305c7c979f8bd1772578934424e7079e8da10d
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-08-24 16:26:56 +0400

    commit

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
+
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name using a function pointer
- * @name: the name to print
- * @f: pointer to a function that takes a char * and returns void
+ * print_name - prints a name using a given function
+ * @name: name to print
+ * @f: pointer to function that takes a char * argument
  */
 void print_name(char *name, void (*f)(char *))
 {
     if (name != NULL && f != NULL)
-    {
         f(name);
-    }
 }
--- function_pointers/README.md
+++ function_pointers/README.md
@@ -1,1 +1,2 @@
-vdnkdvjvn
+vdnkdvjvnnmb
+
