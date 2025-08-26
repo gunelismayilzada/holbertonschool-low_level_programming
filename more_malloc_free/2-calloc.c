a8a9ce9028a17b2cd4f882bec9f7a9e909a62af6
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-08-26 13:06:50 +0400

    commit

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,21 +9,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    unsigned int i;
-    char *ptr;
+	unsigned int i;
+	char *ptr;
 
-    /* Check for zero input */
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    /* Allocate memory */
-    ptr = malloc(nmemb * size);
-    if (ptr == NULL)
-        return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
 
-    /* Set allocated memory to 0 */
-    for (i = 0; i < nmemb * size; i++)
-        ptr[i] = 0;
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
 
-    return (ptr);
+	return (ptr);
 }
