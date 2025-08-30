103f9dba2a114b756929bd35284e62e8eaecf562
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-08-30 17:26:17 +0400

    commit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,6 +14,8 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 /* Function prototype for init_dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
