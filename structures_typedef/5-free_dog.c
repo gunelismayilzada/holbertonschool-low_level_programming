f94514fa7f436e6058b202b1457b8fa2629c995c
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-08-30 17:37:34 +0400

    commit

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees a dog
+ * @d: pointer to the dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	/* Free the dynamically allocated memory for the dog's name and owner */
+	free(d->name);
+	free(d->owner);
+
+	/* Finally, free the dog struct itself */
+	free(d);
+}
