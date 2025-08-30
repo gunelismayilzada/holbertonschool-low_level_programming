4f2c7de3c56ffa94afbd8bbb43098216171a0ef9
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-08-30 17:31:22 +0400

    commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,55 +1,69 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
- * _strlen - return length of string
+ * _strlen - returns length of string
  * @s: the string
+ * 
  * Return: length
  */
 int _strlen(char *s)
 {
     int i = 0;
+
     while (s[i])
         i++;
+
     return (i);
 }
+
 /**
- * _strcpy - copy string from src to dest
+ * _strcpy - copies string from src to dest
  * @dest: destination
  * @src: source
+ * 
  * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
     int i = 0;
+
     while (src[i])
     {
         dest[i] = src[i];
         i++;
     }
+
     dest[i] = '\0';
     return (dest);
 }
+
 /**
  * new_dog - creates a new dog
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
- * Return: pointer to new dog, or NULL
+ * 
+ * Return: pointer to new dog, or NULL if failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *d;
     char *n, *o;
+
     d = malloc(sizeof(dog_t));
     if (d == NULL)
         return (NULL);
+
     n = malloc(_strlen(name) + 1);
     if (n == NULL)
     {
         free(d);
         return (NULL);
     }
+
     _strcpy(n, name);
+
     o = malloc(_strlen(owner) + 1);
     if (o == NULL)
     {
@@ -57,9 +71,12 @@ dog_t *new_dog(char *name, float age, char *owner)
         free(d);
         return (NULL);
     }
+
     _strcpy(o, owner);
+
     d->name = n;
     d->age = age;
     d->owner = o;
+
     return (d);
 }
