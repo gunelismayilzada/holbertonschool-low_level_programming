df4b1ead3f2ea1ffddbb3e2e4b4a93a8f5546427
Your Name <you@example.com>
2025-09-01 14:08:07 +0400

    test

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - Calls a function to print a name.
  * @name: The name to be printed.
- * @f: A pointer to a function that takes a char * and returns void.
+ * @f: A pointer to a function that takes a char * and returns void
  */
 void print_name(char *name, void (*f)(char *))
 {
