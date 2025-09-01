7b5b91bd48194a78a5919c6c2ff2a7696d6c43f1
Your Name <you@example.com>
2025-09-01 14:58:39 +0400

    commit

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,7 +1,6 @@
 #include <stddef.h>
 #include "function_pointers.h"
 
-
 /**
  * int_index - searches for an integer
  * @array: array to search through
@@ -12,16 +11,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
+	int i;
 
-    if (array == NULL || cmp == NULL || size <= 0)
-        return (-1);
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]) != 0)
-            return (i);
-    }
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
 
-    return (-1);
+	return (-1);
 }
