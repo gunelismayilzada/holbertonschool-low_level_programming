44f5ee1ff08cb91b13728907c8025853ca63a7f9
Your Name <you@example.com>
2025-09-01 15:27:52 +0400

    commit

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,35 +2,41 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, error codes on failure
+ */
 int main(int argc, char *argv[])
 {
-    int a, b, result;
-    int (*op_func)(int, int);
+	int a, b, result;
+	int (*op_func)(int, int);
 
-    if (argc != 4) /* Check number of arguments */
-    {
-        printf("Error\n");
-        return 98;
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
-    op_func = get_op_func(argv[2]); /* Get function pointer based on operator */
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
 
-    if (!op_func) /* Check for invalid operator */
-    {
-        printf("Error\n");
-        return 99;
-    }
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
 
-    if (((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)) /* Check divide/modulo by zero */
-    {
-        printf("Error\n");
-        return 100;
-    }
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
 
-    result = op_func(a, b);
-    printf("%d\n", result);
-
-    return 0;
+	result = op_func(a, b);
+	printf("%d\n", result);
+	return (0);
 }
