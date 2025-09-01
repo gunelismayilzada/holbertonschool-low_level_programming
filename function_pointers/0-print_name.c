85c3eb6dad0cfc8aeb0a79011a16ba6ed57fca6b
Your Name <you@example.com>
2025-09-01 14:15:01 +0400

    commit

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
 #include "function_pointers.h"
+#include <stddef.h>  //  Required for NULL
 
 /**
  * print_name - Calls a function to print a name.
  * @name: The name to be printed.
- * @f: A pointer to a function that takes a char * and returns void
+ * @f: A pointer to a function that takes a char * and returns void.
  */
 void print_name(char *name, void (*f)(char *))
 {
     if (name != NULL && f != NULL)
-        return;
-
-    f(name);
+        f(name);
 }
