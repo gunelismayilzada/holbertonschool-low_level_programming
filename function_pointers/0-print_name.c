184c9f00e970b2ce3e19f17820f0069c9f8f1cc0
Your Name <you@example.com>
2025-09-04 14:46:27 +0400

    commit

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,27 @@
+#include <stdio.h>
 #include "function_pointers.h"
-#include <stddef.h> /* Required for NULL */
 
 /**
- * print_name - Calls a function to print a name.
- * @name: The name to be printed.
- * @f: A pointer to a function that takes a char * and returns void.
+ * print_upper - Prints name in uppercase
+ * @name: The name to print
  */
-void print_name(char *name, void (*f)(char *))
+void print_upper(char *name)
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	while (*name)
+	{
+		putchar(toupper(*name));
+		name++;
+	}
+	putchar('\n');
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_name("Alice", print_upper);
+	return (0);
 }
