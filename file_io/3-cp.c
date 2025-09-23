9e22c9aa991e36c02b33d5b0d4a7d3b425801cec
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-09-23 11:51:22 +0400

    commit

---
+++ file_io/3-cp.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * print_error_and_exit - Prints an error message to stderr and exits.
+ * @code: The exit code.
+ * @msg: The error message format string.
+ * @arg: The argument for the format string.
+ */
+void print_error_and_exit(int code, const char *msg, const char *arg)
+{
+	dprintf(STDERR_FILENO, msg, arg);
+	exit(code);
+}
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	/* Open the source file for reading */
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	/* Open (or create) the destination file for writing, truncate if exists */
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		print_error_and_exit(99, "Error: Can't write to %s\n", argv[2]);
+	}
+
+	/* Copy the file contents in chunks */
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			close(fd_from);
+			close(fd_to);
+			print_error_and_exit(99, "Error: Can't write to %s\n", argv[2]);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
