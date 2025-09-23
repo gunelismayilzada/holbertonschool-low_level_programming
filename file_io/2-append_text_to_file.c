babc0e8c4e69a4475368dc711b48890d4e0dea58
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-09-23 11:48:21 +0400

    commit

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: Name of the file to append to.
+ * @text_content: Null-terminated string to add at the end of the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len = 0, written;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* Open file for writing only, don't create if it doesn't exist */
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+		len = strlen(text_content);
+
+	if (len > 0)
+	{
+		written = write(fd, text_content, len);
+		if (written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -6,6 +6,7 @@
 /* read_textfile fonksiyon prototipi */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 /* _putchar fonksiyon prototipi */
 int _putchar(char c);
