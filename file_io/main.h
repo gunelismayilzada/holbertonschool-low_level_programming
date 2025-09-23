040360add5fc40f0345050cac97704c6320c9b15
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-09-23 11:34:02 +0400

    commit

---
+++ file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>  /* For ssize_t */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+
+#endif /* MAIN_H */
