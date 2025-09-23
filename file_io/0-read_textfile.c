c06726da3b10b2dfa29381e48626db0cfb3a3c0b
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-09-23 11:43:35 +0400

    commit

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,38 +13,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t read_bytes, written_bytes;
-    char *buffer;
+	int		fd;
+	ssize_t	read_bytes, written_bytes;
+	char	*buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    read_bytes = read(fd, buffer, letters);
-    if (read_bytes == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	read_bytes = read(fd, buffer, letters);
+	if (read_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
-    free(buffer);
-    close(fd);
+	written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+	free(buffer);
+	close(fd);
 
-    if (written_bytes != read_bytes)
-        return (0);
+	if (written_bytes != read_bytes)
+		return (0);
 
-    return (written_bytes);
+	return (written_bytes);
 }
