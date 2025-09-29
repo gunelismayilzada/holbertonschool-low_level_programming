c0c1f06e435d76b319d81b88fa38b3fa368afdcd
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-09-29 23:40:25 +0400

    commit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,114 +1,69 @@
-#define _GNU_SOURCE
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include "main.h"
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
-/**
- * close_errchk - closes a file descriptor and prints
- * an error message if it fails
- *
- * @fd: file descriptor to close
- *
- * Return: 0 on success, -1 on failure
- */
-int close_errchk(int fd)
-{
-	int err;
 
-	err = close(fd);
-	if (err == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		return (100);
-	}
-	return (0);
+/**
+ * error_exit - Prints an error message and exits with a specific code
+ * @code: Exit code
+ * @message: Error message format
+ * @arg: Argument to format into message
+ */
+void error_exit(int code, const char *message, const char *arg)
+{
+	dprintf(STDERR_FILENO, message, arg);
+	exit(code);
 }
 
 /**
- * write_err - error handler for a write error
- *
- * @fd1: first descriptor to close
- * @fd2: second descriptor to close
- * @filename: filename prompting the error
- *
- * Return: 99
+ * main - Copies the content of a file to another file
+ * @ac: Argument count
+ * @av: Argument vector
+ * Return: 0 on success, exits with specific codes on failure
  */
-int write_err(int fd1, int fd2, char *filename)
+int main(int ac, char **av)
 {
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-	close_errchk(fd1);
-	close_errchk(fd2);
-	return (99);
-}
-
-/**
- * read_err - error handler for a read error
- *
- * @fd1: first descriptor to close
- * @fd2: second descriptor to close
- * @filename: filename prompting the error
- *
- * Return: 98
- */
-int read_err(int fd1, int fd2, char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-	close_errchk(fd1);
-	close_errchk(fd2);
-	return (98);
-}
-
-/**
- * main - copy one file to another, new file with perms 664
- * usage - cp file_from file_to
- *
- * @ac: number of arg
- * @av: list of args
- *
- * Return: 97 if incorrect num of args
- * 98 if file_from does not exist or unreadable
- * 99 if write fails
- * 100 if file close fails
- * 0 otherwise
- */
-int main(int ac, char *av[])
-{
-	char buf[1024];
-	int lenr, lenw, file_from, file_to, err;
+	int file_from, file_to, r, w;
+	char buffer[1024];
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
-	}
+		error_exit(97, "Usage: cp file_from file_to\n", "");
+
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			av[1]);
-		return (98);
-	}
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
-		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+
+	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close_errchk(file_from);
-		return (99);
+		close(file_from);
+		error_exit(99, "Error: Can't write to %s\n", av[2]);
 	}
-	do {
-		lenr = read(file_from, buf, 1024);
-		if (lenr == -1)
-			return (read_err(file_from, file_to, av[1]));
-		lenw = write(file_to, buf, lenr);
-		if (lenw == -1 || lenw != lenr)
-			return (write_err(file_from, file_to, av[2]));
-	} while (lenr == 1024);
-	err = close_errchk(file_from);
-	err += close_errchk(file_to);
-	if (err != 0)
-		return (100);
+
+	while ((r = read(file_from, buffer, 1024)) > 0)
+	{
+		w = write(file_to, buffer, r);
+		if (w != r)
+		{
+			close(file_from);
+			close(file_to);
+			error_exit(99, "Error: Can't write to %s\n", av[2]);
+		}
+	}
+
+	if (r == -1)
+	{
+		close(file_from);
+		close(file_to);
+		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+	}
+
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+
 	return (0);
 }
