9f3f31e0e99d5c0a544df04462ab28cb4e011661
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-09-30 00:08:28 +0400

    commit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,60 +4,41 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
-/**
- * close_errchk - closes a file descriptor and prints
- * an error message if it fails
- *
- * @fd: file descriptor to close
- *
- * Return: 0 on success, -1 on failure
- */
-int close_errchk(int fd)
-{
-	int err;
+#include <stdio.h> /* dprintf üçün icazə verilib */
+/* #include "main.h" - Sizin header faylınız burada olmalıdır */
 
-	err = close(fd);
-	if (err == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		return (100);
-	}
-	return (0);
+/**
+ * exit_with_error - Prints an error message to stderr and exits.
+ * @code: The exit code (97, 98, 99, 100).
+ * @msg: The base error message string.
+ * @file: The file name or FD value string/integer for formatting.
+ */
+void exit_with_error(int code, const char *msg, char *file)
+{
+    /* * Bu funksiya 97, 98 və 99 xətaları üçün istifadə olunur.
+     * 100 xətası xüsusi format tələb edir, onu ayrı idarə edəcəyik.
+     */
+    if (code == 97)
+        dprintf(STDERR_FILENO, "%s\n", msg);
+    else if (code == 98)
+        dprintf(STDERR_FILENO, "%s %s\n", msg, file);
+    else if (code == 99)
+        dprintf(STDERR_FILENO, "%s %s\n", msg, file);
+    
+    exit(code);
 }
 
 /**
- * write_err - error handler for a write error
- *
- * @fd1: first descriptor to close
- * @fd2: second descriptor to close
- * @filename: filename prompting the error
- *
- * Return: 99
+ * close_file - closes a file descriptor and handles error 100.
+ * @fd: file descriptor to close.
  */
-int write_err(int fd1, int fd2, char *filename)
+void close_file(int fd)
 {
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-	close_errchk(fd1);
-	close_errchk(fd2);
-	return (99);
-}
-
-/**
- * read_err - error handler for a read error
- *
- * @fd1: first descriptor to close
- * @fd2: second descriptor to close
- * @filename: filename prompting the error
- *
- * Return: 98
- */
-int read_err(int fd1, int fd2, char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-	close_errchk(fd1);
-	close_errchk(fd2);
-	return (98);
+    if (close(fd) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+        exit(100);
+    }
 }
 
 /**
@@ -67,48 +48,53 @@ int read_err(int fd1, int fd2, char *filename)
  * @ac: number of arg
  * @av: list of args
  *
- * Return: 97 if incorrect num of args
- * 98 if file_from does not exist or unreadable
- * 99 if write fails
- * 100 if file close fails
- * 0 otherwise
+ * Return: 0 on success.
  */
 int main(int ac, char *av[])
 {
-	char buf[1024];
-	int lenr, lenw, file_from, file_to, err;
+    char buf[1024];
+    int lenr, lenw, file_from, file_to;
 
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
-	}
-	file_from = open(av[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			av[1]);
-		return (98);
-	}
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
-		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close_errchk(file_from);
-		return (99);
-	}
-	do {
-		lenr = read(file_from, buf, 1024);
-		if (lenr == -1)
-			return (read_err(file_from, file_to, av[1]));
-		lenw = write(file_to, buf, lenr);
-		if (lenw == -1 || lenw != lenr)
-			return (write_err(file_from, file_to, av[2]));
-	} while (lenr == 1024);
-	err = close_errchk(file_from);
-	err += close_errchk(file_to);
-	if (err != 0)
-		return (100);
-	return (0);
+    if (ac != 3)
+        exit_with_error(97, "Usage: cp file_from file_to", NULL);
+
+    /* 1. Open file_from (Read Error Check) */
+    file_from = open(av[1], O_RDONLY);
+    if (file_from == -1)
+        exit_with_error(98, "Error: Can't read from file", av[1]);
+
+    /* 2. Open file_to (Write Error Check) */
+    file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (file_to == -1)
+    {
+        close_file(file_from); /* Fail fast on close 100 */
+        exit_with_error(99, "Error: Can't write to", av[2]);
+    }
+
+    /* 3. Copy Loop */
+    while ((lenr = read(file_from, buf, 1024)) > 0)
+    {
+        lenw = write(file_to, buf, lenr);
+        
+        if (lenw == -1 || lenw != lenr)
+        {
+            close_file(file_from); /* Fail fast on close 100 */
+            close_file(file_to);   /* Fail fast on close 100 */
+            exit_with_error(99, "Error: Can't write to", av[2]);
+        }
+    }
+    
+    /* 4. Final Read Error Check (lenr == -1) */
+    if (lenr == -1)
+    {
+        close_file(file_from); /* Fail fast on close 100 */
+        close_file(file_to);   /* Fail fast on close 100 */
+        exit_with_error(98, "Error: Can't read from file", av[1]);
+    }
+
+    /* 5. Close Files (Handles Error 100 internally) */
+    close_file(file_from);
+    close_file(file_to);
+
+    return (0);
 }
