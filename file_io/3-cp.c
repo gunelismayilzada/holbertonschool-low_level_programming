d374b8e5bbc7a009901d50f63a741342af04b958
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-09-30 00:22:16 +0400

    commit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-char *create_buffer(char *file);
-void close_file(int fd);
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
@@ -16,7 +15,6 @@ char *create_buffer(char *file)
 	char *buffer;
 
 	buffer = malloc(sizeof(char) * 1024);
-
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
@@ -36,10 +34,10 @@ void close_file(int fd)
 	int c;
 
 	c = close(fd);
-
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO,
+			"Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -69,31 +67,47 @@ int main(int argc, char *argv[])
 
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
+		exit(98);
+	}
+
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", argv[2]);
+		free(buffer);
+		close_file(from);
+		exit(99);
+	}
 
-	do {
-		if (from == -1 || r == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
-
+	while ((r = read(from, buffer, 1024)) > 0)
+	{
 		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		if (w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
+			close_file(from);
+			close_file(to);
 			exit(99);
 		}
+	}
 
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (r > 0);
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
+		close_file(from);
+		close_file(to);
+		exit(98);
+	}
 
 	free(buffer);
 	close_file(from);
