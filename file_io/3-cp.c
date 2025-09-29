84be095884ec54e573681b4cea409369e4465f2c
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-09-29 23:56:25 +0400

    commit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,69 +1,110 @@
-#include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdio.h>    /* For dprintf - though it's technically a system-level function */
+#include <stdlib.h>   /* For exit */
+#include <unistd.h>   /* For read, write, close */
+#include <fcntl.h>    /* For open and file control options */
+#include "main.h"     /* Your custom header file */
+
+/*
+ * Note: You can use dprintf, which is available via <stdio.h>
+ * or directly as a system call in some environments. Since the
+ * requirements allow it, we will use it for error output.
+ * The constants STDIN_FILENO, STDOUT_FILENO, and STDERR_FILENO
+ * are defined in <unistd.h>
+ */
+
+#define BUF_SIZE 1024
 
 /**
- * error_exit - Prints an error message and exits with a specific code
- * @code: Exit code
- * @message: Error message format
- * @arg: Argument to format into message
+ * error_exit - Helper function to print an error message and exit.
+ * @code: The exit code.
+ * @format: The format string for dprintf.
+ * @arg: The argument for the format string (usually a filename or fd).
  */
-void error_exit(int code, const char *message, const char *arg)
+void error_exit(int code, const char *format, const char *arg)
 {
-	dprintf(STDERR_FILENO, message, arg);
-	exit(code);
+    /* Use dprintf to print the error message to POSIX standard error (fd 2) */
+    dprintf(STDERR_FILENO, format, arg);
+    exit(code);
 }
 
 /**
- * main - Copies the content of a file to another file
- * @ac: Argument count
- * @av: Argument vector
- * Return: 0 on success, exits with specific codes on failure
+ * main - Copies the content of a file to another file.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line arguments.
+ *
+ * Return: 0 on success, or an exit code on failure.
  */
-int main(int ac, char **av)
+int main(int argc, char *argv[])
 {
-	int file_from, file_to, r, w;
-	char buffer[1024];
+    int fd_from, fd_to;
+    ssize_t r_count, w_count;
+    char buffer[BUF_SIZE];
+    char *file_from, *file_to;
 
-	if (ac != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", "");
+    /* 1. Check argument count */
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
 
-	file_from = open(av[1], O_RDONLY);
-	if (file_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+    file_from = argv[1];
+    file_to = argv[2];
 
-	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to == -1)
-	{
-		close(file_from);
-		error_exit(99, "Error: Can't write to %s\n", av[2]);
-	}
+    /* 2. Open file_from for reading (O_RDONLY) */
+    fd_from = open(file_from, O_RDONLY);
+    if (fd_from == -1)
+    {
+        /* Error: Can't read from file */
+        error_exit(98, "Error: Can't read from file %s\n", file_from);
+    }
 
-	while ((r = read(file_from, buffer, 1024)) > 0)
-	{
-		w = write(file_to, buffer, r);
-		if (w != r)
-		{
-			close(file_from);
-			close(file_to);
-			error_exit(99, "Error: Can't write to %s\n", av[2]);
-		}
-	}
+    /*
+     * 3. Open/Create file_to for writing.
+     * O_CREAT: Create the file if it does not exist.
+     * O_WRONLY: Open for writing only.
+     * O_TRUNC: Truncate to length 0 if the file already exists.
+     * The permissions are set to rw-rw-r-- (0664).
+     */
+    fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (fd_to == -1)
+    {
+        /* Error: Can't write to file_to (creation failed) */
+        error_exit(99, "Error: Can't write to %s\n", file_to);
+    }
 
-	if (r == -1)
-	{
-		close(file_from);
-		close(file_to);
-		error_exit(98, "Error: Can't read from file %s\n", av[1]);
-	}
+    /* 4. Copy loop: read 1024 bytes, then write */
+    while ((r_count = read(fd_from, buffer, BUF_SIZE)) > 0)
+    {
+        w_count = write(fd_to, buffer, r_count);
 
-	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+        if (w_count == -1 || w_count != r_count)
+        {
+            /* Check for write error or if not all bytes were written */
+            error_exit(99, "Error: Can't write to %s\n", file_to);
+        }
+    }
 
-	if (close(file_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+    /* Check if the loop terminated due to a read error */
+    if (r_count == -1)
+    {
+        error_exit(98, "Error: Can't read from file %s\n", file_from);
+    }
 
-	return (0);
+    /* 5. Close file descriptors, handling errors */
+    if (close(fd_from) == -1)
+    {
+        /* Must convert fd_from to a string for dprintf */
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+        exit(100);
+    }
+
+    if (close(fd_to) == -1)
+    {
+        /* Must convert fd_to to a string for dprintf */
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+        exit(100);
+    }
+
+    return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,103 +1,14 @@
-#include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/stat.h>
+#ifndef MAIN_H
+#define MAIN_H
+ 
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+/* Function Prototypes */
+/* Include any function prototypes you create here, e.g.: */
+/* int main(int argc, char *argv[]); */
 
-/**
- * close_errchk - closes a file descriptor and prints
- * an error message if it fails
- * @fd: file descriptor to close
- * Return: 0 on success, 100 on failure
- */
-int close_errchk(int fd)
-{
-	int err;
+/* If you need _putchar for other tasks, include it here: */
+/* int _putchar(char c); */
 
-	err = close(fd);
-	if (err == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		return (100);
-	}
-	return (0);
-}
-
-/**
- * write_err - error handler for a write error
- * @fd1: first descriptor to close
- * @fd2: second descriptor to close
- * @filename: filename prompting the error
- * Return: 99
- */
-int write_err(int fd1, int fd2, char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-	close_errchk(fd1);
-	close_errchk(fd2);
-	return (99);
-}
-
-/**
- * read_err - error handler for a read error
- * @fd1: first descriptor to close
- * @fd2: second descriptor to close
- * @filename: filename prompting the error
- * Return: 98
- */
-int read_err(int fd1, int fd2, char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-	close_errchk(fd1);
-	close_errchk(fd2);
-	return (98);
-}
-
-/**
- * main - copy one file to another, new file with perms 664
- * usage: cp file_from file_to
- * @ac: number of args
- * @av: list of args
- * Return: 0 on success, exits with codes on error
- */
-int main(int ac, char *av[])
-{
-	char buf[1024];
-	int lenr, lenw, file_from, file_to, err;
-
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
-	}
-	file_from = open(av[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		return (98);
-	}
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
-		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close_errchk(file_from);
-		return (99);
-	}
-	do {
-		lenr = read(file_from, buf, 1024);
-		if (lenr == -1)
-			return (read_err(file_from, file_to, av[1]));
-		lenw = write(file_to, buf, lenr);
-		if (lenw == -1 || lenw != lenr)
-			return (write_err(file_from, file_to, av[2]));
-	} while (lenr == 1024);
-
-	err = close_errchk(file_from);
-	err += close_errchk(file_to);
-	if (err != 0)
-		return (100);
-
-	return (0);
-}
+#endif /* MAIN_H */
