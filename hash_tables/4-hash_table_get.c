15042d38421bd3d6c9b61897bbcf79e2b49448c8
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-09-30 23:24:19 +0400

    commit

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -2,14 +2,12 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - retrieves a value associated with a key
- * @ht: pointer to the hash table
- * @key: the key to search for
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: Pointer to the hash table
+ * @key: The key to search for
  *
- * Return: the value associated with the key, or NULL if not found
+ * Return: The value associated with the key, or NULL if not found
  */
-
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
