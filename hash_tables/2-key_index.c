3505a9d5ebbdbf98a2c90bb69407d1d520436869
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-09-30 21:24:48 +0400

    commit

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -7,8 +7,6 @@
  *
  * Return: the index at which the key/value pair should be stored
  */
-
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
