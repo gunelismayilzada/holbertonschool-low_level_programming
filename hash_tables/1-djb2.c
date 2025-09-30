8fda821f57fe7b012d5ee4b4e1dc4c747bf17af3
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-09-30 21:15:48 +0400

    commit

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -6,7 +6,6 @@
  *
  * Return: the calculated hash as an unsigned long int
  */
-
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash = 5381;
@@ -14,6 +13,5 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
 	return (hash);
 }
