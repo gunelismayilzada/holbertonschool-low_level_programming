be44affcd539a2e888ce65ea34e8e5e2ec017c4d
gunelismayilzada <gunelismayilzad7@gmail.com>
2025-10-05 20:35:34 +0400

    commit

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -7,25 +7,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *node, *tmp;
-    unsigned long int i;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        while (node != NULL)
-        {
-            tmp = node->next;
-            free(node->key);
-            free(node->value);
-            free(node);
-            node = tmp;
-        }
-    }
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
 
-    free(ht->array);
-    free(ht);
+	free(ht->array);
+	free(ht);
 }
